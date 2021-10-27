4e24b6ec5dfa895104bd8a6716e5219fc07b26b8
francisco <gatupancho@gmail.com>
2021-10-27 17:03:31 -0300

    build: inicio del proyecto

---
+++ proyecto/Taximetro/src/Taximetro.c
@@ -0,0 +1,14 @@
+/*
+===============================================================================
+ Name        : Taximetro.c
+ Author      : Grupo 1
+ Description : Un taximetro hecho para el integrador de Digitales 3
+===============================================================================
+*/
+
+#include "LPC17xx.h"
+
+int main(void) {
+
+    return 0 ;
+}
