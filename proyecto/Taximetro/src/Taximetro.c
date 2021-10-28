58731ba395e9a68a6306291d88a8d8e9b83349d4
francisco <gatupancho@gmail.com>
2021-10-28 00:12:58 -0300

    build:falta UART y DMA

--- proyecto/Taximetro/src/Taximetro.c
+++ proyecto/Taximetro/src/Taximetro.c
@@ -9,12 +9,20 @@
 #include "LPC17xx.h"
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_pinsel.h"
+#include "lpc17xx_adc.h"
+#include "lpc17xx_timer.h"
+#include "lpc17xx_clkpwr.h"
+
+
+
 
 void confGPIO(void); // Prototipo de la funcion de conf. de puertos
-void confADC(void);
+void configADC(void);
 void config_timer(void);
 void confExtInt(void); //Prototipo de la funcion de conf. de interrupciones externas
 void change_state(uint8_t state);
+void config_timer_1(void);
+
 
 void rutina_1(void); // poner un nombre mas representativo
 void rutina_2(void);
@@ -33,6 +41,10 @@ uint8_t get_TeclaMatrical(void);
 #define CHANNEL_0 0
 #define LEDS_RED (1 << 4)
 #define LEDS_GREEN (1 << 5)
+#define BUZZER (1 << 6)
+
+
+#define TIMEMUESTREO 1
 
 #define EDGE_RISING 0
 #define LENGTH 16
@@ -44,7 +56,7 @@ uint8_t get_TeclaMatrical(void);
 #define OCUPADO 2
 #define STOP  3
 
-uint8_t modo = 0;
+uint8_t modo = LIBRE;
 uint16_t tarifa = 0;
 uint16_t distancia = 0;
 uint8_t car_state = 0;
@@ -60,6 +72,10 @@ int main(void)
 {
 	confGPIO();
 	confExtInt();
+	configADC();
+	config_timer();
+	config_timer_1();
+
 
 	while(1)
 	{
@@ -85,7 +101,8 @@ int main(void)
 void rutina_1(void)
 {
 	change_state(LIBRE);
-	LPC_SC->PCONP &= ~(1 << 1) | ~(1 << 2) | ~(1 << 12); // se apagan los timer y el adc
+	TIM_Cmd(LPC_TIM0, DISABLE);
+	TIM_Cmd(LPC_TIM1, DISABLE);
 
 	tarifa = 0;
 
@@ -98,10 +115,10 @@ void rutina_1(void)
 void rutina_2(void)
 {
 	change_state(OCUPADO);
-	LPC_SC->PCONP |= (1 << 1); // se prende el timer0
+	TIM_ResetCounter(LPC_TIM0);
+	TIM_Cmd(LPC_TIM0, ENABLE);
 
 	tarifa = 0;
-
 	while (modo == OCUPADO)
 	{
 		if (distancia > 200)
@@ -114,7 +131,8 @@ void rutina_2(void)
 }
 void rutina_3(void)
 {
-	LPC_SC->PCONP &= ~(1 << 1) | ~(1 << 2) | ~(1 << 12); // se apagan los timer y el adc
+	TIM_Cmd(LPC_TIM0, DISABLE);
+	TIM_Cmd(LPC_TIM1, DISABLE);
 
 	while (modo == STOP)
 	{
@@ -127,11 +145,13 @@ void change_state(uint8_t state)
 {
 	if (state == LIBRE)
 	{
-		GPIO_SetValue(PINSEL_PORT_0, (LEDS_GREEN || ~(LEDS_RED)));
+		GPIO_SetValue(PINSEL_PORT_0, (LEDS_GREEN));
+		GPIO_ClearValue(PINSEL_PORT_0, (LEDS_RED));
 	}
 	if (state == OCUPADO)
 	{
-		GPIO_SetValue(PINSEL_PORT_0, (~LEDS_GREEN || (LEDS_RED)));
+		GPIO_SetValue(PINSEL_PORT_0, (LEDS_RED));
+		GPIO_ClearValue(PINSEL_PORT_0, (LEDS_GREEN));
 	}
 
 	return;
@@ -176,76 +196,91 @@ void confGPIO(void)
 	PinCfg.Pinnum = 5;
 	PINSEL_ConfigPin(&PinCfg);
 
-	GPIO_SetDir(PINSEL_PORT_0, (LEDS_RED || LEDS_GREEN), OUTPUT);
+	PinCfg.Pinnum = 6;
+	PINSEL_ConfigPin(&PinCfg);
+
+	GPIO_SetDir(PINSEL_PORT_0, (LEDS_RED | LEDS_GREEN | BUZZER), OUTPUT);
 	FIO_ByteSetValue(2, 0, OUT_TECLADO);
+
+	GPIO_ClearValue(PINSEL_PORT_0, (BUZZER));
 
 	return;
 }
 void configADC(void)
 {
-    ///CONFIGURACION PIN AD0.0///
-    PINSEL_CFG_Type conf_pin;
-    conf_pin.Funcnum = PINSEL_FUNC_1;            //PIN EN MODO AD0.0
-    conf_pin.Portnum = PINSEL_PORT_1;            //PUERTO 0
-    conf_pin.Pinnum = 23;                        //PIN 23
-    conf_pin.Pinmode = PINSEL_PINMODE_TRISTATE;    //
+	PINSEL_CFG_Type conf_pin;
+	conf_pin.Funcnum = PINSEL_FUNC_1;            //PIN EN MODO AD0.0
+	conf_pin.Portnum = PINSEL_PORT_0;            //PUERTO 0
+	conf_pin.Pinnum = 23;                        //PIN 23
+	conf_pin.Pinmode = PINSEL_PINMODE_TRISTATE;    //
     PINSEL_ConfigPin(&conf_pin);
 
+	CLKPWR_SetPCLKDiv(CLKPWR_PCLKSEL_ADC, 3);
+	CLKPWR_ConfigPPWR(CLKPWR_PCONP_PCAD, ENABLE);
     ///CONFIGURACION ADC///
 
     ADC_Init(LPC_ADC, F_MUESTREO); //ENCIENDO ADC
     ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0,ENABLE); //POR CANAL 0
     ADC_StartCmd(LPC_ADC, ADC_START_ON_MAT01);    //START CON MATCH, DEBE MUESTREAR CADA 1 SEG
     ADC_EdgeStartConfig(LPC_ADC, ADC_START_ON_RISING);
-    ADC_IntConfig(LPC_ADC, ADC_ADINTEN0, ENABLE);    //INTERRUPCION HABILITADA POR CANAL 0
+	ADC_BurstCmd(LPC_ADC, DISABLE);
+	ADC_IntConfig(LPC_ADC, ADC_ADGINTEN, SET);
 
+	ADC_GlobalGetStatus(LPC_ADC, 1);
     NVIC_EnableIRQ(ADC_IRQn);
 
     return;
 }
 void config_timer(void)
 {
+	CLKPWR_SetPCLKDiv(CLKPWR_PCLKSEL_TIMER0, CLKPWR_PCLKSEL_CCLK_DIV_4);
+	CLKPWR_ConfigPPWR(CLKPWR_PCONP_PCTIM0, ENABLE);
+
     TIM_TIMERCFG_Type    struct_config;
     TIM_MATCHCFG_Type    struct_match;
 
-    struct_config.PrescaleOption    =    TIM_PRESCALE_TICKVAL;
-    struct_config.PrescaleValue     =    0;
+    struct_config.PrescaleOption    =    TIM_PRESCALE_USVAL;
+    struct_config.PrescaleValue     =    1000;
 
     struct_match.MatchChannel       =    1;
     struct_match.IntOnMatch         =    DISABLE; //deshabilitamos las interrupciones por timer
     struct_match.ResetOnMatch       =    ENABLE;//resetea el contador del timer cuando se produce un match
     struct_match.StopOnMatch        =    DISABLE; //no detiene el contador del timer cuando se produce un match
     struct_match.ExtMatchOutputType =    TIM_EXTMATCH_TOGGLE;
-    struct_match.MatchValue         =    49999999;
+    struct_match.MatchValue         =    5;
 
     TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &struct_config); //se prende el timer0, se configura la division del clock del periferico, y se
                                                         //configura el timer como modo temporizador y ademas se retesea el contador y se lo saca del reset
     TIM_ConfigMatch(LPC_TIM0, &struct_match); //carga todas las configuraciones del struct_match en ls registros correspondientes
 
-    TIM_Cmd(LPC_TIM0, ENABLE); //habilita el contador del timer y prescaler
+    TIM_ResetCounter(LPC_TIM0);
+    TIM_Cmd(LPC_TIM0, DISABLE); //habilita el contador del timer y prescaler
 
     return;
 }
 void config_timer_1(void)
 {
+	CLKPWR_ConfigPPWR(CLKPWR_PCONP_PCTIM1, ENABLE);
+
     TIM_TIMERCFG_Type    struct_config;
     TIM_MATCHCFG_Type    struct_match;
 
-    struct_config.PrescaleOption    =    TIM_PRESCALE_TICKVAL;
-    struct_config.PrescaleValue     =    9;
+    struct_config.PrescaleOption    =    TIM_PRESCALE_USVAL;
+    struct_config.PrescaleValue     =    1000000;
 
     struct_match.MatchChannel       =    0;
     struct_match.IntOnMatch         =    ENABLE; //deshabilitamos las interrupciones por timer
     struct_match.ResetOnMatch       =    ENABLE;//resetea el contador del timer cuando se produce un match
     struct_match.StopOnMatch        =    DISABLE; //no detiene el contador del timer cuando se produce un match
     struct_match.ExtMatchOutputType =    TIM_EXTMATCH_NOTHING;
-    struct_match.MatchValue         =    600000000;
+    struct_match.MatchValue         =    60;
 
     TIM_Init(LPC_TIM1, TIM_TIMER_MODE, &struct_config); //se prende el timer1, se configura la division del clock del periferico, y se
                                                         //configura el timer como modo temporizador y ademas se retesea el contador y se lo saca del reset
     TIM_ConfigMatch(LPC_TIM1, &struct_match); //carga todas las configuraciones del struct_match en ls registros correspondientes
 
-    TIM_Cmd(LPC_TIM1, ENABLE); //habilita el contador del timer y prescaler
+    TIM_ResetCounter(LPC_TIM1);
+    TIM_Cmd(LPC_TIM1, DISABLE); //habilita el contador del timer y prescaler
 
     return;
 }
@@ -261,9 +296,12 @@ void confExtInt(void)
 }
 void EINT3_IRQHandler(void)
 {
+	GPIO_SetValue(PINSEL_PORT_0, (BUZZER));
 	uint8_t tecla = get_TeclaMatrical();
+	for(uint64_t i=0; i<100000 ; i++){}
+	GPIO_ClearValue(PINSEL_PORT_0, (BUZZER));
 
-	(tecla> 0 && tecla <= 3) ? modo = teclado_matricial[tecla] : modo = modo ;
+	(tecla >= 0 && tecla < 3) ? (modo = teclado_matricial[tecla]) : (modo = modo) ;
 
 	GPIO_ClearInt(PINSEL_PORT_2, IN_TECLADO);
 
@@ -274,7 +312,7 @@ uint16_t Convertir_Distancia(uint16_t adc_value)
 	uint16_t velocidad = 0;
 	if (adc_value > 0)
 	{
-		velocidad = (adc_value * VELOCIDAD) / RESOLUCION;
+		velocidad = (adc_value * VELOCIDAD_MAX) / RESOLUCION;
 		TIM_Cmd(LPC_TIM1, DISABLE);
 
 	}
@@ -293,7 +331,9 @@ void ADC_IRQHandler(void)
 
 	ADC0Value = ADC_ChannelGetData(LPC_ADC, ADC_CHANNEL_0);
 
-	distancia = Convertir_Distancia(ADC0Value) * TimeMuestreo;
+	distancia += Convertir_Distancia(ADC0Value) * TIMEMUESTREO;
+
+	ADC_GlobalGetStatus(LPC_ADC, 1);
 
 	return;
 }
